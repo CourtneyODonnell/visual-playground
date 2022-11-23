b2e2808983d03638fc53af73883493bd6de86448
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-11-22 20:19:48 -0400

    added instructions

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
        <>
          <div className="header">
-          
             <h3>MealBuddy</h3>
+            <span>--logo options to be placed here--</span>
             
          </div>
       <div className="App">
@@ -20,8 +20,50 @@ function App() {
         </div>
         <div className="mainpage">
           <h1>Home Page "/"</h1>
+          <p>Welcome to the Visual Playground. Currently all items render in this one page (just scroll down). Feel free to add any mock-ups for styling of any component you're working on. Please add new component mock-ups in the App.js file in the return section with a label. Please add all styling to App.css</p>
         </div>
       </div> 
+
+      <section>
+        <h1>Sign-up</h1>
+  
+      </section>
+
+      <section>
+        <h1>Sign-in</h1>
+      </section>
+
+      <section>
+        <h1>User logged in view to replace log-in sign-up once user is signed-in</h1>
+        <p>Welcome Back, Username!</p>
+      </section>
+
+      <section>
+        <h1>Menu - full Table</h1>
+
+      </section>
+
+      <section>
+        <h1>Grocery List</h1>
+
+      </section>
+      <section>
+        <h1>Grocery List pop-up prompt upon 'save' to text user custom list</h1>
+      </section>
+        <h1>Settings - account and dietary settings</h1>
+
+      <section>
+        <h1>next component</h1>
+      </section>
+
+      <section>
+        <h1>next component</h1>
+      </section>
+
+      <section>
+        <h1>next component</h1>
+      </section>
+
     </>
   );
 }
