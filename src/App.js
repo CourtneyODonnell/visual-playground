56b5f8fe31d465f2417685d5f5285c19e948ce0c
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-11-22 20:05:32 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+
+function App() {
+	
+  return (
+       <>
+         <div className="header">
+          
+            <h3>MealBuddy</h3>
+            
+         </div>
+      <div className="App">
+        <div className="sidebar">
+          <p className="sidebar-text">Menu</p>
+          <br />
+          <p className="sidebar-text">Grocery List</p>
+          <br />
+          <p className="sidebar-text">Settings</p>
+
+        </div>
+        <div className="mainpage">
+          <h1>Home Page "/"</h1>
+        </div>
+      </div> 
+    </>
+  );
+}
+
+
+export default App;
+
+
